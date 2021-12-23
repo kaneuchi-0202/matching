b6a33bc5ad4b6e2742043fe7b05e070d350c4588
kazuki <kaneuchi.0202@gmail.com>
2021-12-23 23:28:41 +0900

    create index page sample.

--- matching/matching/settings.py
+++ matching/matching/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'matching_web.apps.MatchingWebConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
